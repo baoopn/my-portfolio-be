267a80b20e3ab3415e601cbc08bc0bf9a0411cbc
Bao Nguyen <pbnguyen@ualberta.ca>
2025-03-01 14:53:40 -0700

    initialize Spring Boot project with basic structure and HelloWorld endpoint

---
+++ src/main/java/com/baoopn/my_portfolio_be/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.baoopn.my_portfolio_be.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
